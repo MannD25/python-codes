9e45c827cb60712b25a4dfbc7322bc241a1c9cbe
MannD25 <77035743+MannD25@users.noreply.github.com>
2025-01-07 15:47:59 +0530

    Add files via upload

---
+++ mainn.py
@@ -0,0 +1,50 @@
+a=int(input("enter your number "))
+b=3
+#1
+print(a+b)
+#2
+print(b-a)
+#3
+print(b*a)
+#4
+print(b/a)
+
+#6
+hours=int(input("enter the hours"))
+print("minutes =" ,hours*60)
+#7
+minutes=int(input("enter minutes"))
+print("hours=",minutes/60)
+#8
+dollar=int(input("enter dollar"))
+print("rupees =",dollar*48)
+#9
+rupees=int(input("enter rupees"))
+print("dollar=",rupees/48)
+#10
+dollar=int(input("enter dollar"))
+pound=(dollar*70)/48
+print("pound is ",pound)
+#11
+grams=int(input("enter the grams"))
+print("kg is ",grams/1000)
+#12
+ks=int(input("enter the kgrams"))
+print("grams is ",ks*1000)
+#13
+bytes=int(input("enter bytes"))
+kb=bytes/1024
+mb=kb/1024
+gb=mb/1024
+print("kb is ",kb,"gb is ",gb,"mb is ",mb)
+#24
+a=int(input("enter a "))
+b=int(input("enter b"))
+c=a
+a=b
+b=c
+print("a now is ",a,"b now is ",b)
+
+
+
+
