c975e65e33de3afdf01444f7631391dce41bcbef
Maan Desai <manndesai25@gmail.com>
2025-02-26 01:22:31 +0530

    commit 1e3f4b3d1b1b3

--- dict4.py
+++ dict4.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 str=input("enter your string:")
 d=dict()
 count=0
@@ -17,25 +16,3 @@ for i in str:
         
 
 print(d)
-=======
-
-user_input = input("Enter the string: ")
-
-char_list = list(user_input)
-
-char_frequency = {}
-
-for i in range(len(char_list)):
-    if char_list[i] is None:
-        continue
-    count = 1
-    for j in range(i + 1, len(char_list)):
-        if char_list[i] == char_list[j]:
-            count += 1
-            char_list[j] = None
-    char_frequency[char_list[i]] = count
-
-print("Character frequencies:")
-for char, freq in char_frequency.items():
-    print(f"'{char}': {freq}")
->>>>>>> 9788cd2c4ad663a52592b6fabb96a7e22157bf59
